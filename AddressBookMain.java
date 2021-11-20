3e2e6375a41f0cb77ee878d93d3687a0dcc0e469
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-20 17:00:00 +0530

    Main Class

---
+++ AddressBookMain.java
@@ -0,0 +1,50 @@
+package sampleaddress;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+     AddressBookMain addressBook = new AddressBookMain();
+		
+		MultipleAddressBooks multiAddressBook = new MultipleAddressBooks();
+		
+		while (true) {
+			System.out.println("Enter \n 1. Add new AddressBook\n 2. Add contact in AddressBook\n " +
+					"3. Edit the contact in AddressBook\n 4. Delete the contact in AddressBook\n 5. Delete the AddressBook\n " +
+					"6. Print the AddressBook\n 7. Print the contacts in AddressBook\n 0. Exit Applcation");
+			Scanner scanner = new Scanner(System.in);
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				multiAddressBook.addAddressBook();
+				break;
+			case 2:
+				multiAddressBook.addContact();
+				break;
+			case 3:
+				multiAddressBook.editContactInBook();
+				break;
+			case 4:
+				multiAddressBook.deleteContactInBook();
+				break;
+			case 5:
+				multiAddressBook.deleteAddressBook();
+				break;
+			case 6:
+				multiAddressBook.printBook();
+				break;
+			case 7:
+				multiAddressBook.printContactsInBook();
+				break;
+			case 0:
+				System.out.println("Exit Application");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Enter the wrong input");
+			}
+		}
+	}
+}
+
