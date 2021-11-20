3b7e8a4b863149684af440f3ae3f2c54bb3902ba
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-20 12:28:38 +0530

    Main Class

---
+++ AddressBookMain.java
@@ -0,0 +1,36 @@
+package sampleaddress;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the AddressBook System");
+		Scanner sc = new Scanner(System.in);
+		Contact contact = new Contact();
+		
+		while (true) {
+			System.out.println("Enter \n 1 To add The contact\n 2  To Edit \n 3 delete contact \n 0 To exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+				case 1:
+					contact.addContact();
+					break;
+				case 2:
+					contact.editContact();
+					break;
+				case 3:
+					contact.deleteContact();
+					break;
+				case 0:
+					System.out.println("Exit from application");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid input");
+					continue;
+			}
+		}
+	}
+}
+
