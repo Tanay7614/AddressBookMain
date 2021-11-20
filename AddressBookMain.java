5347d32eb39f922214c8c0796f7034c8b1826279
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-20 11:47:23 +0530

    Add files via upload

---
+++ AddressBookMain.java
@@ -0,0 +1,30 @@
+package sampleaddress;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the AddressBook System");
+		Scanner sc = new Scanner(System.in);
+		Contact contact = new Contact();
+		
+		while (true) {
+			System.out.println("Enter \n 1 To add The contact \n 0 To exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+				case 1:
+					contact.addContact();
+					break;
+				case 0:
+					System.out.println("Exit from application");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid input");
+					continue;
+			}
+		}
+	}
+}
+
